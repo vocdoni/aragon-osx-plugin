8b7910bf713bc1b0791465d3914cb22ecc531ec3
Jordi Pinyana <jordi.pinyana.paga@gmail.com>
2023-09-15 03:16:45 +0200

    make the main contract smaller

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -60,7 +60,7 @@ const config: HardhatUserConfig = {
     settings: {
       optimizer: {
         enabled: true,
-        runs: 1000000,
+        runs: 2000,
       },
       outputSelection: {
         '*': {
